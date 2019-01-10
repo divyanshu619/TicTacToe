2b704fac2a1368a8149dafb7fbbf310ca344d5bb
Divyanshu <divyanshu619@gmail.com>
2019-01-10 23:40:04 +0530

    Updated script to copy dll files

--- maked.bat
+++ maked.bat
@@ -1,2 +1,2 @@
-copy "D:\horse raaz\TTT\TTTlib\bin\Debug\TTTlib.dll" "D:\horse raaz\TTT\TicTacToe\bin\Debug"
-copy "D:\horse raaz\TTT\TTTlib\dllmain.h" "D:\horse raaz\TTT\TicTacToe"
+copy ".\TTTlib\bin\Debug\TTTlib.dll" ".\TicTacToe\bin\Debug"
+copy ".\TTTlib\dllmain.h" ".\TicTacToe"
